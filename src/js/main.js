d6f73bb7b2f7e79ac57c65565e049d715d430585
Kirill <kkoloskov02@gmail.com>
2020-10-01 15:35:37 +0300

    chaged timeout

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,17 @@
 import tabs from './modules/tabs';
-import modal, {
-    openModal
-} from './modules/modal';
+import modal from './modules/modal';
 import timer from './modules/timer';
 import cards from './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
 import slider from './modules/slider';
+import {
+    openModal
+} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
     const modalTimerId = setTimeout(() =>
-        openModal('.modal', modalTimerId), 5000);
+        openModal('.modal', modalTimerId), 10000);
 
     tabs('.tabheader__item',
         '.tabcontent',
